668620bf17d0b18f33338e6ec4135dc47fc7bcec
XimenZangief <s9671073@gmail.com>
2022-01-03 09:29:09 +0800

    獨立header.php和footer.php header.php 新增圖片連結標題+回首頁連結 footer.php 更新頁尾年分 index.php 新增跑馬燈&改變寬度

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 ﻿<!-- 網頁設計乙級題組二*時限版* -->
 
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -15,17 +14,12 @@
 </head>
 
 <body>
-	<div id="alerr"
-		style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 		<pre id="ssaa"></pre>
 	</div>
-	<iframe name="back" style="display:none;"></iframe>
-	<div id="all">
-		<div id="title">
-			00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
-		<div id="title2">
 
-		</div>
+	<div id="all">
+		<?php include "./front/header.php" ?>
 		<div id="mm">
 			<div class="hal" id="lef">
 				<a class="blo" href="?do=po">分類網誌</a>
@@ -36,7 +30,7 @@
 			</div>
 			<div class="hal" id="main">
 				<div>
-
+					<marquee style="width:80%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
 					<span style="width:18%; display:inline-block;">
 						<a href="?do=login">會員登入</a>
 					</span>
@@ -45,11 +39,7 @@
 				</div>
 			</div>
 		</div>
-		<div id="bottom">
-			本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
-			<br>
-			服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
-		</div>
+		<?php include "./front/footer.php" ?>
 	</div>
 
 </body>
