605c8fe63c88d6614db7405b1ac8d409e60a0553
XimenZangief <s9671073@gmail.com>
2022-01-05 11:36:15 +0800

    匯入base.php 新增動態今日瀏覽&累計瀏覽 使用DW製作home.php內容(插入-Spry標籤面板) 登入系統版型預先建立

--- front/header.php
+++ front/header.php
@@ -1,5 +1,8 @@
 <div id="title">
-    <?= date("m 月 d 號 l"); ?> | 今日瀏覽: 1 | 累積瀏覽: 36
+    <!-- <?=dd($_SESSION['view']);?> -->
+    <?= date("m 月 d 號 l"); ?> |
+    今日瀏覽: <?= $Views->find(['date' => date("Y-m-d")])['total']; ?> |
+    累積瀏覽: <?= $Views->math('sum', 'total'); ?>
     <a href="index.php" style='float:right;'>回首頁</a>
 </div>
 <div id="title2">
