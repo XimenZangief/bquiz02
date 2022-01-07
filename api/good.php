b904c0ed2aabe49d1890cdf4d51cfd645eefc70c
XimenZangief <s9671073@gmail.com>
2022-01-07 20:25:24 +0800

    收回讚是$Log->del啦捧U

--- api/good.php
+++ api/good.php
@@ -7,7 +7,7 @@ $type=$_POST['type'];
 switch ($type) {
     case 1:
         //收回讚
-        $Logs->save(['news'=>$news['id'],'user'=>$_SESSION['login']]);
+        $Logs->del(['news'=>$news['id'],'user'=>$_SESSION['login']]);
 
         // 文章的good欄位--
         $news['good']--;
